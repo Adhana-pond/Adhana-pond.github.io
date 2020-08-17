9ad008dce85992962b34092f09dceef44181986f
AdhanaPond <dhanapond@gmail.com>
2020-08-17 01:51:04 +0000

    added logo, pictures, changed font sizes and aligned them

--- script.js
+++ script.js
@@ -6,8 +6,38 @@ function changePicture(pictureNumber){
   var image = document.getElementById("myImage");
   if (pictureNumber ===1){ 
     image.src= 
-    "https://www.grammy.com/sites/com/files/styles/news_detail_header/public/news/96306227.jpg?itok=soyTlYAt"
+    "pictureone.JPG"
   }else{
-    image.src= "https://data.whicdn.com/images/155762797/original.jpg"
+    image.src= "picturetwo.JPG"
+  }
+}
+
+function changePicture1(pictureNumber1){
+  var image1 = document.getElementById("myImage1");
+  if (pictureNumber1 ===1){ 
+    image1.src= 
+    "picturethree.JPG"
+  }else{
+    image1.src= "picturefour.JPG"
+  }
+}
+
+function changePicture2(pictureNumber2){
+  var image2 = document.getElementById("myImage2");
+  if (pictureNumber2 ===1){ 
+    image2.src= 
+    "picturefive.JPG"
+  }else{
+    image2.src= "picturesix.JPG"
+  }
+}
+
+function changePicture3(pictureNumber3){
+  var image3 = document.getElementById("myImage3");
+  if (pictureNumber3 ===1){ 
+    image3.src= 
+    "pictureseven.JPG"
+  }else{
+    image3.src= "pictureeight.JPG"
   }
 }
